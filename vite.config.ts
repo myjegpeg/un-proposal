ff8a534c617718957b2090eed2301639f9331e76
myjegpeg <myjegpeg@gmail.com>
2024-09-11 21:06:52 -0300

    feat: add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,51 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['logo.png', 'logo2.png', 'logo3.png'],
+      manifest: {
+        name: 'Carteirinha Digital UFF',
+        short_name: undefined,
+        description: 'Proposta para Carteirinha Digital Universitária',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#f1f8fe',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/pwa-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/pwa-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
